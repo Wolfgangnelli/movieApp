bc63b13892711d5e1f9321bd9e34e95fded98f40
Wolfgangnelli <39460849+Wolfgangnelli@users.noreply.github.com>
2020-05-09 18:30:41 +0200

    Add files via upload

---
+++ precache-manifest.547c7a01693ff6e85ee2aaf476f4e59c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae49c2d36cf00346e5f3bae85a479248",
+    "url": "/movieApp/index.html"
+  },
+  {
+    "revision": "3733d37741c14d5bac3c",
+    "url": "/movieApp/static/css/main.20c0086b.chunk.css"
+  },
+  {
+    "revision": "229791f514c9ab576cae",
+    "url": "/movieApp/static/js/2.22d6fc82.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movieApp/static/js/2.22d6fc82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3733d37741c14d5bac3c",
+    "url": "/movieApp/static/js/main.fd1bb090.chunk.js"
+  },
+  {
+    "revision": "754927047f1cbe7d5a59",
+    "url": "/movieApp/static/js/runtime-main.06a8e946.js"
+  }
+]);
