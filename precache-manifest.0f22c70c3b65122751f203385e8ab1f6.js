c2494e950bb8e2d5306cf1117a71f33c8eee9d32
Andrea Solfanelli <39460849+Wolfgangnelli@users.noreply.github.com>
2021-07-16 15:43:01 +0200

    update build folder

---
+++ precache-manifest.0f22c70c3b65122751f203385e8ab1f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0262cdabed1f8943bc845e22ec5aefda",
+    "url": "/movieApp/index.html"
+  },
+  {
+    "revision": "f303cb9d309f6b3ceade",
+    "url": "/movieApp/static/css/main.20c0086b.chunk.css"
+  },
+  {
+    "revision": "8bf5010811300e89a3bd",
+    "url": "/movieApp/static/js/2.1452bbba.chunk.js"
+  },
+  {
+    "revision": "ddfd09a9397ea336a2550edb6da27f94",
+    "url": "/movieApp/static/js/2.1452bbba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f303cb9d309f6b3ceade",
+    "url": "/movieApp/static/js/main.10c2daa7.chunk.js"
+  },
+  {
+    "revision": "754927047f1cbe7d5a59",
+    "url": "/movieApp/static/js/runtime-main.06a8e946.js"
+  }
+]);
